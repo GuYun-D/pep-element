b30842027837ff4fb1de31221209d26920a5b422
solitary-cloud <1946306647@qq.com>
2022-06-27 20:07:18 +0800

    feat(choose-area): 完成省市区三里联动组件

--- src/components/chooseArea/src/index.vue
+++ src/components/chooseArea/src/index.vue
@@ -1,24 +1,123 @@
 <template>
   <div>
-    <el-select v-model="province" placeholder="请选择省份">
-      <el-option>1</el-option>
+    <el-select clearable v-model="province" placeholder="请选择省份">
+      <el-option
+        :value="item.code"
+        v-for="item in areas"
+        :key="item.code"
+        :label="item.name"
+      ></el-option>
     </el-select>
-    <el-select style="margin: 0 10px" v-model="city" placeholder="请选择城市">
-      <el-option>2</el-option>
+    <el-select
+      :disabled="!province"
+      style="margin: 0 10px"
+      v-model="city"
+      clearable
+      placeholder="请选择城市"
+    >
+      <el-option
+        :key="item.code"
+        :label="item.name"
+        v-for="item in selectCity"
+        :value="item.code"
+      ></el-option>
     </el-select>
-    <el-select v-model="area" placeholder="请选择区域">
-      <el-option>3</el-option>
+    <el-select
+      :disabled="!province || !city"
+      v-model="area"
+      clearable
+      placeholder="请选择区域"
+    >
+      <el-option
+        :key="item.code"
+        :label="item.name"
+        v-for="item in seleteArea"
+        :value="item.code"
+      ></el-option>
     </el-select>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue-demi";
+import { watch, ref } from "vue-demi";
 import allArea from "../lib/pca-code.json";
 
-const province = ref("");
-const city = ref("");
-const area = ref("");
+export interface AreaItem {
+  name: string;
+  code: string;
+  children?: AreaItem;
+}
+
+export interface Data {
+  name: string;
+  code: string;
+}
+
+const emites = defineEmits(["change"]);
+
+const province = ref<string>("");
+const city = ref<string>("");
+const area = ref<string>("");
+const areas = ref(allArea);
+
+const selectCity = ref<AreaItem[]>([]);
+const seleteArea = ref<AreaItem[]>([]);
+
+watch(
+  () => province.value,
+  (val) => {
+    if (val) {
+      selectCity.value = areas.value.find(
+        (item) => item.code === province.value
+      )!.children!;
+    }
+    city.value = "";
+    area.value = "";
+  }
+);
+
+watch(
+  () => city.value,
+  (val) => {
+    if (val) {
+      seleteArea.value = selectCity.value.find(
+        (item) => item.code === city.value
+      )!.children!;
+    }
+    area.value = "";
+  }
+);
+
+watch(
+  () => area.value,
+  (val) => {
+    if (!val) {
+      return;
+    }
+    const provinceData: Data = {
+      code: province.value,
+      name:
+        province.value &&
+        allArea.find((item) => item.code === province.value)!.name,
+    };
+    const cityData: Data = {
+      code: city.value,
+      name:
+        city.value &&
+        selectCity.value.find((item) => item.code === city.value)!.name,
+    };
+    const areaData: Data = {
+      code: val,
+      name: val && seleteArea.value.find((item) => item.code === val)!.name,
+    };
+
+    emites("change", {
+      province: provinceData,
+      city: cityData,
+      area: areaData,
+    });
+  }
+);
 </script>
 
 <style lang="scss" scoped>
--- src/views/chooseArea/index.vue
+++ src/views/chooseArea/index.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <ChooseArea></ChooseArea>
+    <ChooseArea @change="handleGetAreas"></ChooseArea>
   </div>
 </template>
 
 <script setup lang="ts">
 import ChooseArea from "../../components/chooseArea/src/index.vue";
+
+const handleGetAreas = (val: any) => {
+  console.log(val);
+};
 </script>
