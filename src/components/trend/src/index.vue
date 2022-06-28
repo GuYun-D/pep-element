f0dd71bfe512079251d470647fc5007cc3c70a45
solitary-cloud <1946306647@qq.com>
2022-06-28 21:25:21 +0800

    feat(trend): 完成趋势组件的封装
    useSlot

--- src/components/trend/src/index.vue
+++ src/components/trend/src/index.vue
@@ -1,28 +1,58 @@
 <template>
   <div class="trend">
-    <div class="text">
-      {{ text }}
+    <div
+      class="text"
+      :style="{
+        color: textColor,
+      }"
+    >
+      <slot v-if="slot.default"></slot>
+      <div v-else>{{ text }}</div>
     </div>
     <div class="icon">
-      <el-icon-arrowup
-        :style="{ color: upIconColor }"
+      <!-- <el-icon-arrowup
+        :style="{ color: !reverseColor ? upIconColor : '#52c41a' }"
         v-if="iconType === 'up'"
       ></el-icon-arrowup>
       <el-icon-arrowdown
         :style="{
-          color: downIconColor,
+          color: !reverseColor ? downIconColor : '#f5222d',
         }"
         v-else
-      ></el-icon-arrowdown>
+      ></el-icon-arrowdown> -->
+      <component
+        v-if="iconType === 'up'"
+        :is="`el-icon-${toLine(upIcon)}`"
+        :style="{ color: !reverseColor ? upIconColor : '#52c41a' }"
+      ></component>
+      <component
+        v-else
+        :is="`el-icon-${toLine(downIcon)}`"
+        :style="{
+          color: !reverseColor ? downIconColor : '#f5222d',
+        }"
+      ></component>
+
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed, useSlots } from "vue";
+import { toLine } from "../../../utils";
+
 const props = defineProps({
   iconType: {
     type: String,
     default: "up",
+  },
+  upIcon: {
+    type: String,
+    default: "ArrowUp",
+  },
+  downIcon: {
+    type: String,
+    default: "ArrowDown",
   },
   text: {
     type: String,
@@ -36,7 +66,29 @@ const props = defineProps({
     type: String,
     default: "#52c41a",
   },
+  upTextColor: {
+    type: String,
+    default: "rgb(0, 0, 0)",
+  },
+  downTextColor: {
+    type: String,
+    default: "rgb(0, 0, 0)",
+  },
+  /**
+   * 颜色反转只在默认的颜色下生效，如果使用了自定义颜色，该属性将不生效
+   */
+  reverseColor: {
+    type: Boolean,
+    default: false,
+  },
 });
+
+const slot = useSlots();
+
+const textColor = computed(() => {
+  return props.iconType === "up" ? props.upTextColor : props.downTextColor;
+});
+
 </script>
 
 <style lang="scss" scoped>
--- src/views/trend/index.vue
+++ src/views/trend/index.vue
@@ -1,14 +1,30 @@
 <template>
   <div>
-    <trend text="营业额" up-icon-color="#f40"></trend>
+    <trend text="营业额" up-icon-color="#f40">插槽</trend>
     <trend text="营业额" iconType="down" downIconColor="#39b1bb"></trend>
+    <trend
+      text="营业额"
+      reverseColor
+      iconType="down"
+      downIconColor="#39b1bb"
+    ></trend>
+    <trend
+      text="自定义up"
+      up-icon="CaretTop"
+      iconType="up"
+      downIconColor="#39b1bb"
+    ></trend>
+    <trend
+      text="自定义down"
+      iconType="down"
+      down-icon="CaretBottom"
+      downIconColor="#39b1bb"
+    ></trend>
   </div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style lang="scss" scoped>
-
 </style>
