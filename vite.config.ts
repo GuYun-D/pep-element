807e28dc33630cddbe6766f1a6e33ad8ed97cc80
solitary-cloud <1946306647@qq.com>
2022-06-30 13:55:12 +0800

    refactor(vite.config.ts): 添加对tsx的支持🎨

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
   // 修改端口号
   server: {
-    port: 8080
+    port: 8080,
+    open: true
   }
 })
